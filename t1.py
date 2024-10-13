803baec54647cfab86800a993b4f712adb9fcfc2
alexmiloeski <a.miloeski@gmail.com>
2024-10-13 19:12:23 +0200

    feat: implement embedding and comparing with other images

--- t1.py
+++ t1.py
@@ -24,6 +24,24 @@ def load_base_embeddings():
 def compare_to_other_images(other_image_filenames):
     print("Loading and processing other images...")
 
+    other_images = []
+    for other_image in other_image_filenames:
+        other_images.append(Image.open(other_image))
+
+    other_inputs = processor(images=other_images, return_tensors="pt", padding=True)
+
+    with torch.no_grad():
+        other_embeddings = model.get_image_features(**other_inputs)
+
+    # calculate cosine similarity to base image for each "other image"
+    cos_sim = torch.nn.functional.cosine_similarity(base_embeddings, other_embeddings, dim=1)
+
+    # convert to percentage for better printout
+    similarity_percentages = (cos_sim + 1) / 2 * 100
+
+    for i, similarity in enumerate(similarity_percentages):
+        print(f"Similarity with {other_image_filenames[i]}: {similarity.item():.2f}%")
+
 ###
 
 print("start")
