26539e0f4a4809eee8c8f45838573541f94a5133
alexmiloeski <a.miloeski@gmail.com>
2024-10-13 19:08:32 +0200

    feat: add function skeletons

--- t1.py
+++ t1.py
@@ -2,12 +2,35 @@ from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
 
+###
+
+BASE_IMAGE_PATH = "base.jpg"
+BASE_EMBEDDINGS_PATH = "base_embeddings.pt"
 MODEL_NAME = "openai/clip-vit-large-patch14"
+
+###
+
+def load_base_embeddings():
+    return None
+
+###
+
+def compare_to_other_images(other_image_filenames):
+    print("Loading and processing other images...")
+
+###
 
 print("start")
 
 # load CLIP model
 model = CLIPModel.from_pretrained(MODEL_NAME)
 processor = CLIPProcessor.from_pretrained(MODEL_NAME)
+
+base_embeddings = load_base_embeddings()
+print("Creating new base embeddings...")
+
+###
+
+compare_to_other_images(["image2.jpg", "image3.jpg"])
 
 print("end")
