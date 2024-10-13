b5de62733e3f913dedce8c666c8a1c067414a6a3
alexmiloeski <a.miloeski@gmail.com>
2024-10-13 19:08:12 +0200

    feat: load clip model and processor

--- t1.py
+++ t1.py
@@ -2,6 +2,12 @@ from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
 
+MODEL_NAME = "openai/clip-vit-large-patch14"
+
 print("start")
+
+# load CLIP model
+model = CLIPModel.from_pretrained(MODEL_NAME)
+processor = CLIPProcessor.from_pretrained(MODEL_NAME)
 
 print("end")
